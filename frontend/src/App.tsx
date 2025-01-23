3c0cda2c13fcc3694c62d6812939a52af676e309
Tyler <tylersora@gmail.com>
2025-01-22 22:56:44 -0700

    Add footer with address and links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'; // Import the Footer component
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Shop from './pages/Shop';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/shop" element={<Shop />} />
-        </Routes>
-      </Container>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Navbar />
+        <Container sx={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </Container>
+        <Footer /> {/* Add Footer component here */}
+      </Box>
     </Router>
   );
 };
