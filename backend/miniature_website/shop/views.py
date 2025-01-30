d53b2f0489ccfd43209ceef1574ca7495b3be0f1
Tyler <tylersora@gmail.com>
2025-01-29 21:29:28 -0700

    Change email

--- backend/miniature_website/shop/views.py
+++ backend/miniature_website/shop/views.py
@@ -11,7 +11,7 @@ def send_email_view(request):
     data = request.data
     subject = f"Contact Form Submission from {data.get('name') + ' ' + data.get('phone')}"
     body = data.get('message')  # Extract the message field for the body
-    to = 'tylersora@gmail.com'  # Use your email address here
+    to = 'theminiaturemanager@gmail.com'  # Use your email address here
     from_email = data.get('email')
 
     # Use Django's send_mail function
