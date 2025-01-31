22bc571f9e974fcfd47ef272ee1dd086472cdebd
Tyler <tylersora@gmail.com>
2025-01-30 22:57:18 -0700

    Fix gh pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/miniature-website/',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
